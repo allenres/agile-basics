7766db6b0662e86df55d18e9af7d2c0d88585e20
allenres <allen.resulidze@gmail.com>
2026-04-21 11:42:52 -0700

    hero component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header';
+import Hero from './components/Hero';
 import data from './data.json'
 function App() {
   console.log(data)
   return (
     <>
       <Header labelTitle={data.label}/>
+      <Hero tagline={data.tagline} city={data.city} founded={data.founded}/>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,14 @@
 import './Hero.css';
-export default function Hero() {
-    
+export default function Hero({tagline, city, founded}) {
+    return (
+        <div className='hero'>
+            <div>
+                <h2 className='tagline'>{tagline}</h2>
+                <p className='about'>We're a records label based in {city}. Founded in the year {founded}.</p>
+            </div>
+            <div className='img-container'>
+                <img src="https://lirp.cdn-website.com/e00ccc9d/dms3rep/multi/opt/Vinyl-Records-on-Shelf-652x435-3142589a-640w.jpg" alt="" />
+            </div>
+        </div>
+    )
 }
