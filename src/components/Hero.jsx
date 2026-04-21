11cfcccd4ce7f4fec851a15476c1e4adb00ce0fe
allenres <allen.resulidze@gmail.com>
2026-04-21 11:52:28 -0700

    mobile responsiveness hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ export default function Hero({tagline, city, founded}) {
     return (
         <div className='hero'>
             <div>
-                <h2 className='tagline'>{tagline}</h2>
-                <p className='about'>We're a records label based in {city}. Founded in the year {founded}.</p>
+                <h2 className='tagline'>{tagline}</h2> 
+                <p className='about'>We're a small independent label based out of {city}. Founded in the year {founded}.</p>
             </div>
             <div className='img-container'>
                 <img src="https://lirp.cdn-website.com/e00ccc9d/dms3rep/multi/opt/Vinyl-Records-on-Shelf-652x435-3142589a-640w.jpg" alt="" />
