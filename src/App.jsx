265a571f0db7c657c55d95ef41e6adea85102ddd
allenres <allen.resulidze@gmail.com>
2026-04-21 11:23:56 -0700

    Header component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/Header';
 import data from './data.json'
 function App() {
   console.log(data)
   return (
     <>
+      <Header labelTitle={data.label}/>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,10 @@
 import './Header.css';
-export default function Header() {
-    
+export default function Header({labelTitle}) {
+    return (
+        <>
+            <header className='header'>
+                <h1 className='title'>{labelTitle}</h1>
+            </header>
+        </>
+    )
 }
