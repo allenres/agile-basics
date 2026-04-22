1b6e5dc8475f1b93d4c33b8d39d8d15ab6be7df3
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-04-22 11:06:32 -0700

    Added artist card component

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,4 +1,10 @@
 import './ArtistCard.css';
-export default function ArtistCard() {
-    
+export default function ArtistCard({artist}) {
+    return (
+        <div className='artist-card'>
+            <h1>{artist.name}</h1>
+            <p>{artist.genre}</p>
+            <p>{artist.bio}</p>
+        </div>
+    )
 }
--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -2,9 +2,13 @@ import './ArtistList.css';
 import ArtistCard from './ArtistCard'
 
 export default function ArtistList({artists}) {
+    const artistCards = artists.map((el) => {
+        return <ArtistCard artist={el} />
+    })
+
     return (
-        artists.map((el) => {
-            <ArtistCard artist={el}/>
-        })
+        <div className='artist-list'>
+            {artistCards}
+        </div>
     )
 }
