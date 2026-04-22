014f81165f7594882f39fa358985262c8c4a2f57
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-04-22 11:15:14 -0700

    Added Album item component

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,4 +1,9 @@
 import './AlbumItem.css';
-export default function AlbumItem() {
-    
+export default function AlbumItem({album}) {
+    return (
+        <div className='album-items'>
+            <p>{album.title}</p>
+            <p>{album.year}</p>
+        </div>
+    )
 }
--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,10 +1,17 @@
 import './ArtistCard.css';
+import AlbumItem from './AlbumItem'
+
 export default function ArtistCard({artist}) {
+    const albums = artist.albums.map((abl) => {
+        return <AlbumItem album={abl}/>
+    })
+
     return (
         <div className='artist-card'>
             <h1>{artist.name}</h1>
             <p>{artist.genre}</p>
             <p>{artist.bio}</p>
+            {albums}
         </div>
     )
 }
