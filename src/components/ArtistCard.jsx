7f987cb6714a3c2ea1b49e9b29f2b4f766ae7eb1
Akaioka <brendanvillaraza@yahoo.com>
2026-05-07 10:23:44 -0700

    Test

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,6 +1,6 @@
 import './ArtistCard.css';
 import AlbumItem from './AlbumItem'
-
+//test
 export default function ArtistCard({artist}) {
     const albums = artist.albums.map((abl) => {
         return <AlbumItem album={abl}/>
